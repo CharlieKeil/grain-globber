69ae9655bfaee62d524f14b8a4786679560a5c5a
CharlieKeil <Keil0009@algonquinlive.com>
2016-03-15 13:31:27 -0400

    Added all content

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var $ul = $('ul');
+
+grains.forEach(function (grain)
+{
+  var $li = $('ul');
+  var $img = $('<img>');
+  var $h2 = ('<h2>');
+  var $p = $('<p>');
+
+  $h2.html(grain.name);
+  $img.attr('src', 'images/' + grain.img);
+  $p.html(grain.desc);
+
+  $li.append($img, $h2, $p);
+  $ul.append($li);
+
+});
